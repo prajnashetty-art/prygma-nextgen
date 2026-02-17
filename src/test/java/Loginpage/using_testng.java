e3bcdaa170fe55c61a2bebf88aa23b210b17fb01
Prajna Shetty <Prajna Shetty@LAPTOP-9SLE8TDA>
2026-02-17 15:44:34 +0530

    second commit

--- src/test/java/Loginpage/using_testng.java
+++ src/test/java/Loginpage/using_testng.java
@@ -9,12 +9,28 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 public class using_testng {
 
 	WebDriver driver;
+	ExtentReports extent;
+	ExtentTest test;
+	
 	@BeforeMethod
 	public void setup() throws Exception
 	{
+		extent=new ExtentReports();
+		ExtentSparkReporter spark=new ExtentSparkReporter("Report_3.html");
+		spark.config().setTheme(Theme.STANDARD);
+		spark.config().setDocumentTitle("Login_using_testNG");
+		extent.attachReporter(spark);
+		
+		test=extent.createTest("Login_Test_TestNG_approach");
+		
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -24,16 +40,25 @@ public class using_testng {
 	@Test
 	public void inputcredentials()
 	{
+		try {
 		driver.findElement(By.id("email")).sendKeys("gocoxir121@fentaoba.com");
 		driver.findElement(By.id("password")).sendKeys("goco@123");
 		driver.findElement(By.id("remember-me")).click();
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		System.out.println("Logged in successfully");
+		test.pass("Logged in successfully");
+		}
+		catch(Exception e)
+		{
+			System.out.println("Login failed");
+			test.fail("Login failed");
+		}
 		
 	}
 	@AfterMethod
 	public void result()
 	{
-		System.out.println("Logged in successfully");
+		extent.flush();
 		driver.quit();
 	}
 	
