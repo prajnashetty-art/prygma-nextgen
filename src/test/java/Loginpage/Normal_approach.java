96f9621140be378ffe5371f7ea3be275968881c1
Prajna Shetty <Prajna Shetty@LAPTOP-9SLE8TDA>
2026-02-17 14:30:58 +0530

    second commit

--- src/test/java/Loginpage/Normal_approach.java
+++ src/test/java/Loginpage/Normal_approach.java
@@ -4,12 +4,27 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 public class Normal_approach {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		ExtentReports extent=new ExtentReports();
+		ExtentSparkReporter spark=new ExtentSparkReporter("Report_1.html");
+		spark.config().setTheme(Theme.DARK);
+		spark.config().setDocumentTitle("Login_normal_approach");
+		extent.attachReporter(spark);
+		
+		ExtentTest test=extent.createTest("Login Test - Normal Approach");
+		
 		ChromeDriver driver=new ChromeDriver();
+		try
+		{
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		driver.get("https://www.prygmanextgen.com/");
@@ -18,9 +33,17 @@ public class Normal_approach {
 		driver.findElement(By.id("password")).sendKeys("goco@123");
 		driver.findElement(By.id("remember-me")).click();
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
-		driver.quit();
 		System.out.println("Logged in successfully");
-		
-	}
-
+		test.pass("Logged in successfully");
+		}
+		catch (Exception e)
+		{
+			System.out.println("Login failed");
+			test.pass("Login failed");
+		}
+		finally {
+		driver.quit();
+		extent.flush();
+		}
+		}
 }
