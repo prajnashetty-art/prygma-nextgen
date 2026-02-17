a63c7f55ba1d535b12ac493f95485a6c10c6d369
Prajna Shetty <Prajna Shetty@LAPTOP-9SLE8TDA>
2026-02-17 15:23:31 +0530

    second commit

--- src/test/java/Loginpage/Cross_browsing.java
+++ src/test/java/Loginpage/Cross_browsing.java
@@ -7,18 +7,41 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 public class Cross_browsing {
 
-
 	WebDriver driver;
+	ExtentReports extent;
+	ExtentTest test;
+	
+	@BeforeMethod
+	public void attachreport()
+	{
+		extent=new ExtentReports();
+		ExtentSparkReporter spark=new ExtentSparkReporter("Report_2.html");
+		spark.config().setTheme(Theme.STANDARD);
+		spark.config().setDocumentTitle("Login_crossbrowsing_approach");
+		extent.attachReporter(spark);
+		
+	}
 	@Test
 	@Parameters("browsers")
 	
 		public void crossbrowsers(String browsername)
 		{
+		   test=extent.createTest("Login_Test_crossbrowsing approach");
+		   
 			if(browsername.equalsIgnoreCase("chrome"))
 			{
 				driver=new ChromeDriver();
@@ -27,7 +50,8 @@ public class Cross_browsing {
 				{
 				driver=new EdgeDriver();
 				}
-			
+			try
+			{
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 			driver.get("https://www.prygmanextgen.com/");
@@ -37,8 +61,21 @@ public class Cross_browsing {
 			driver.findElement(By.id("remember-me")).click();
 			driver.findElement(By.xpath("//button[@type='submit']")).click();
 			System.out.println("Logged in successfully");
+			test.pass("Logged in successfully");
+			}
+			catch(Exception e)
+			{
+				System.out.println("Login failed");
+				test.fail("Login failed");
+			}
+			finally
+			{
 			driver.quit();
-		
-	}
-
+			}
+		}
+			@AfterMethod
+			public void generatereport()
+			{
+				extent.flush();
+			}
 }
